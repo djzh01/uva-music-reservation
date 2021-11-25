f909f3e026753b2fdfe379d2e1d73c1814eb6387
djzh01 <djzhao01@gmail.com>
2021-11-24 20:47:53 -0500

    connect to db resolved

---
+++ db-config.php
@@ -0,0 +1,8 @@
+<?php
+    $loginUser = "dz8pa"; 
+    $loginPass = "carryiousgeorge1";
+    $host = "mysql01.cs.virginia.edu"; // local host
+    $schema = "dz8pa_roomreservation"; // DB Schema
+    $dsn = "mysql:host=$host;dbname=$schema";
+
+?>
