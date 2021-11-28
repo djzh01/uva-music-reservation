3f88cf538efbc594e737f86099ec543ed1ff9033
djzh01 <djzhao01@gmail.com>
2021-11-27 19:41:41 -0500

    fixed urls and return response for invalid login

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+
+
+    <form action="login.php" method="post">
+        <h3>Login</h3>
+        Computing ID: <input type="text" name="computingid">
+        <input type="Submit">
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+    </form>
+
+    <form action="register.html" method="post">
+
+        <input type="Submit" value="register">
+    </form>
+
+
+
+
+    <br /><br />
+</body>
+
+</html>
