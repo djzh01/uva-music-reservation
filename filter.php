22d234cc66721bfada468491d982cb7fa7f503b7
djzh01 <djzhao01@gmail.com>
2021-12-03 00:22:16 -0500

    i hate css so much kms

--- filter.php
+++ filter.php
@@ -19,15 +19,20 @@
 		$typestr = " AND type ='" . $_GET['type'] ."'"; 
 	}
 
+	$weekday = isWeekday($_GET['date']);
+
 	$stmt = $db->prepare("SELECT room_id, start_time 
 	FROM Reservation_times NATURAL JOIN Room
-	WHERE weekday = '" . isWeekday($_GET['date']) . "' 
+	WHERE weekday = '" . $weekday . "' 
 	AND (room_id, start_time) NOT IN (select room_id, time FROM Reserves WHERE date= :date)" . $sizestr . $typestr);
 	
+	$time_stmt = $db->prepare("SELECT room_id, start_time
+	FROM Reservation_times NATURAL JOIN Room
+	WHERE weekday = {$weekday}" . $sizestr . $typestr);
+
 	$stmt->bindValue(':date', $_GET['date'], PDO::PARAM_STR);
 
-	echo "<br>";
-	// $previous = -1;
+	$avail_times = [];
 	$times = [];
 
 	if($stmt) {
@@ -39,36 +44,68 @@
 			else {
 				$times[$row[0]] = [$row[1]];
 			}
-			// if($row[0] == $previous){
-			// 	echo "{$row[1]} ";
-			// 	$previous = $row[0];
-			// }
-			// else{
-			// 	echo "<br>";
-			// 	echo "{$row[0]}, {$row[1]} ";
-			// 	$previous = $row[0];
-			// }
-
 		}	
 	}
-
-	foreach ($times as $room => $time_slots) {
-		echo "<ul>{$room}";
+	if($time_stmt) {
+		$time_stmt->execute();
+		while($row = $time_stmt->fetch()){
+			if(array_key_exists($row[0], $avail_times)){
+				array_push($avail_times[$row[0]], $row[1]);
+			}
+			else {
+				$avail_times[$row[0]] = [$row[1]];
+			}
+		}
+	}
+	foreach ($avail_times as $room => $time_slots) {
+		echo "<ul class=\"vert-btn-group\">{$room}";
 		foreach ($time_slots as $time) {
-			echo "<li>
-
-			<label class=\"btn btn-outline-success\" 
-				for=\"time{$room}{$time}\">
-
-			<input id=\"time{$room}{$time}\" 
-				type=\"radio\" 
-				class=\"btn-check\" 
-				value=\"{$room},{$time},{$_GET['date']}\" 
-				name=\"times\">{$time}</label>
-			</li>";
+			$formatted_time = date_format(date_create($time), 'g:i A');
+			if(in_array($time, $times[$room])){
+				echo "<li class=\"btn btn-outline-success\">
+				<label  
+					for=\"time{$room}{$time}\">
+	
+				<input id=\"time{$room}{$time}\" 
+					type=\"radio\" 
+					class=\"btn-check\" 
+					value=\"{$room},{$time},{$_GET['date']}\" 
+					name=\"times\">{$formatted_time}</label>
+				</li>";
+			}
+			else{
+				echo "<li class=\"btn disabled\">
+				<label class=\"\" 
+					for=\"time{$room}{$time}\">
+	
+				<input id=\"time{$room}{$time}\" 
+					type=\"radio\" 
+					class=\"btn-check disabled\" 
+					value=\"{$room},{$time},{$_GET['date']}\" 
+					name=\"times\" disabled=\"disabled\">{$formatted_time}</label>
+				</li>";
+			}
 		}
 		echo "</ul>";
 	}
+	// foreach ($times as $room => $time_slots) {
+	// 	echo "<ul>{$room}";
+	// 	foreach ($time_slots as $time) {
+	// 		$formatted_time = date_format(date_create($time), 'g:i A');
+
+	// 		echo "<li>
+	// 		<label class=\"btn btn-outline-success\" 
+	// 			for=\"time{$room}{$time}\">
+
+	// 		<input id=\"time{$room}{$time}\" 
+	// 			type=\"radio\" 
+	// 			class=\"btn-check\" 
+	// 			value=\"{$room},{$time},{$_GET['date']}\" 
+	// 			name=\"times\">{$formatted_time}</label>
+	// 		</li>";
+	// 	}
+	// 	echo "</ul>";
+	// }
 
 	echo '<script>',
 	'createactivation()',
